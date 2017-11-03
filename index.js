8cb2c9b70c1eb02fd58e32680940d75ab831b276
Scott Atkins <satkins16@gmail.com>
2017-11-03 13:16:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function lowerCaseDrivers(drivers) {
   return drivers.map(function(driver) { return driver.toLowerCase() })
 }
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) { return })
+}
