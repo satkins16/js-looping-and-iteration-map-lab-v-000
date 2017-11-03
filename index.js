36f37bfffbe92af8dd2a3627801260736fc807ed
Scott Atkins <satkins16@gmail.com>
2017-11-03 13:19:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function lowerCaseDrivers(drivers) {
 function nameToAttributes(drivers) {
   return drivers.map(function(driver) {
     const driverFirst = driver.split(' ')[0]
+    const driverLast = driver.split(' ')[1]
+
+    return { firstName: driverFirst, lastName: driverLast }
   })
 }
